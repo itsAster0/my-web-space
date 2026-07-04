5a3c3d352088c7cb5769974670fda947aa91854f
kryxen <kryxen.dev@gmail.com>
2026-07-04 22:05:14 +0530

    chore:moved to bun

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { mdsvex } from "mdsvex";
-import adapter from "@sveltejs/adapter-static";
+import adapter from "svelte-adapter-bun";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
